f3f711dbe7bae9bec10a2233f2e1826339991252
devsafix <devsafix@gmail.com>
2025-08-08 04:08:24 +0600

    admin layout added

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,7 @@
 import App from "@/App";
+import AdminLayout from "@/components/layout/AdminLayout";
 import About from "@/pages/About";
+import AdminAnalytics from "@/pages/AdminAnalytics";
 import { createBrowserRouter } from "react-router";
 
 export const router = createBrowserRouter([
@@ -10,6 +12,16 @@ export const router = createBrowserRouter([
       {
         path: "about",
         Component: About,
+      },
+    ],
+  },
+  {
+    path: "/admin",
+    Component: AdminLayout,
+    children: [
+      {
+        path: "analytics",
+        Component: AdminAnalytics,
       },
     ],
   },

5140a3642f60b923e2b84db887e838042f652309
devsafix <devsafix@gmail.com>
2025-08-08 03:27:11 +0600

    Project Setup done with ShadCn, vite, Tailwind

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1>Tour Management Client App</h1>
+      <Button>ShadCn</Button>
+    </>
+  );
+}
+
+export default App;
